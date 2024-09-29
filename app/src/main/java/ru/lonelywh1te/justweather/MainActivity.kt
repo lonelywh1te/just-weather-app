e8b4ae6c826d2bb925555d590d76579f5f745f62
lonelywh1te <lonelywh1te@ya.ru>
2024-09-29 15:35:38 +1000

    Initial commit

---
+++ app/src/main/java/ru/lonelywh1te/justweather/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.lonelywh1te.justweather
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
