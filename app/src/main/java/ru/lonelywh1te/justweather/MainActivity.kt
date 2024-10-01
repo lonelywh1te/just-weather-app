79d51258606aba1d85c8499c262ec38691cbc39d
lonelywh1te <lonelywh1te@ya.ru>
2024-10-01 20:52:19 +1000

    feat: Search screen ui

--- app/src/main/java/ru/lonelywh1te/justweather/MainActivity.kt
+++ app/src/main/java/ru/lonelywh1te/justweather/MainActivity.kt
@@ -1,19 +1,22 @@
 package ru.lonelywh1te.justweather
 
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.ActionBar
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import ru.lonelywh1te.justweather.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var inputMethodManager: InputMethodManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         val navController = navHostFragment.navController
@@ -22,24 +25,66 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.toolbarTitle.text = destination.label
+            binding.toolbar.title = null
+
             when (destination.id) {
                 R.id.weatherFragment -> {
-                    setupLocationButton(supportActionBar)
+                    showToolbarTitle()
+                    showLocationButton()
+
+                    binding.toolbarTitle.setOnClickListener {
+                        navController.navigate(R.id.to_searchCityFragment)
+                    }
+                }
+
+                R.id.searchCityFragment -> {
+                    showSearchField()
+                }
+
+                else -> {
+                    binding.toolbarTitle.setOnClickListener(null)
                 }
             }
         }
 
-        binding.toolbar.setOnClickListener {
-            Toast.makeText(this, "Toolbar clicked", Toast.LENGTH_SHORT).show()
-        }
-
         setContentView(binding.root)
+    }
+
+    private fun showToolbarTitle() {
+        binding.toolbarTitle.visibility = View.VISIBLE
+        binding.etSearchField.visibility = View.GONE
+
+        hideKeyboard()
+    }
+
+    private fun showSearchField() {
+        binding.toolbarTitle.visibility = View.GONE
+
+        binding.etSearchField.visibility = View.VISIBLE
+        binding.etSearchField.setOnEditorActionListener { _, actionId, _ ->
+            when(actionId) {
+                EditorInfo.IME_ACTION_SEARCH -> TODO("Not yet implemented")
+                else -> false
+            }
+        }
+        binding.etSearchField.requestFocus()
+        showKeyboard()
+    }
+
+    private fun showKeyboard() {
+        inputMethodManager.showSoftInput(binding.etSearchField, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun hideKeyboard() {
+        inputMethodManager.hideSoftInputFromWindow(binding.etSearchField.windowToken, 0)
+    }
+
+    private fun showLocationButton() {
+        supportActionBar?.let {
+            it.setHomeAsUpIndicator(R.drawable.ic_location)
+            it.setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
 
-private fun setupLocationButton(actionBar: ActionBar?) {
-    if (actionBar == null) return
-
-    actionBar.setHomeAsUpIndicator(R.drawable.ic_location)
-    actionBar.setDisplayHomeAsUpEnabled(true)
-}
--- app/src/main/java/ru/lonelywh1te/justweather/SearchCityFragment.kt
+++ app/src/main/java/ru/lonelywh1te/justweather/SearchCityFragment.kt
@@ -9,15 +9,16 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.MenuProvider
-import ru.lonelywh1te.justweather.databinding.FragmentWeatherBinding
+import androidx.navigation.fragment.findNavController
+import ru.lonelywh1te.justweather.databinding.FragmentSearchCityBinding
 
-
-class WeatherFragment : Fragment(), MenuProvider {
-    private var _binding: FragmentWeatherBinding? = null
+class SearchCityFragment : Fragment(), MenuProvider {
+    private var _binding: FragmentSearchCityBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = FragmentWeatherBinding.inflate(inflater, container, false)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentSearchCityBinding.inflate(inflater, container, false)
         requireActivity().addMenuProvider(this, viewLifecycleOwner)
 
         return binding.root
@@ -29,16 +30,15 @@ class WeatherFragment : Fragment(), MenuProvider {
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-        menuInflater.inflate(R.menu.main_menu, menu)
+        menuInflater.inflate(R.menu.search_menu, menu)
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
         when (menuItem.itemId) {
-            R.id.settings -> {
-                return true
-            }
+            R.id.search -> TODO("Not yet implemented")
+            android.R.id.home -> findNavController().popBackStack()
         }
 
-        return false
+        return true
     }
 }
